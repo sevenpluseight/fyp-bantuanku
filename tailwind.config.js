9eb10da93c32c3de3d45ae9effaa47dd2220a838
Carol Hew <hew78pikrou@gmail.com>
2026-09-20 22:17:26 +0800

    feat: extend Tailwind CSS theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./App.{js,jsx,ts,tsx}",
-      "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./App.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#0352CE",
+                    dark: "#032F81",
+                    light: "#0878EA",
+                },
+
+                accent: {
+                    red: "#F5222D",
+                    orange: "#FD9E12",
+                    yellow: "#FFC21C",
+                },
+
+                background: "#FFFFFF",
+                surface: "#F7F8FA",
+
+                foreground: "#1E1E2E",
+                "muted-foreground": "#626775",
+
+                border: "#E2E5EA",
+            },
+        },
+    },
+    plugins: [],
 };
