2ecd066e757e56def06ca678341eb5fab495f2fa
Carol Hew <hew78pikrou@gmail.com>
2026-09-22 01:49:24 +0800

    feat: add ProfileScreen (WIP)

---
+++ src/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import Screen from "../../components/layout/Screen";
+import ScreenHeader from "../../components/layout/ScreenHeader";
+
+export default function ProfileScreen() {
+  return (
+      <Screen>
+        <ScreenHeader title="Profile" description="Manage your personal and household information." />
+      </Screen>
+  );
+}
