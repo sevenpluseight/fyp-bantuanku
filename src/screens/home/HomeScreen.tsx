49e170d89447a73b27a22976b8f17b1ac47e6c7e
Carol Hew <hew78pikrou@gmail.com>
2026-09-22 01:49:17 +0800

    feat: add HomeScreen (WIP)

---
+++ src/screens/home/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import Screen from "../../components/layout/Screen";
+import ScreenHeader from "../../components/layout/ScreenHeader";
+
+export default function HomeScreen() {
+  return (
+      <Screen>
+        <ScreenHeader title="Home" description="Welcome to BantuanKu" />
+      </Screen>
+  );
+}
