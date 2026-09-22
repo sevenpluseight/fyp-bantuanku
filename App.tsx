9ed44be3cb1bf4e360c34bdd05bd38246cf9ecd0
Carol Hew <hew78pikrou@gmail.com>
2026-09-22 12:14:02 +0800

    feat: integrate navigation with RootNavigator

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import "./global.css";
+
 import { useState } from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import SplashScreen from "./src/screens/splash/SplashScreen";
-import { Text, View } from "react-native";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   const [showSplash, setShowSplash] = useState(true);
@@ -10,19 +13,13 @@ export default function App() {
   return (
       <SafeAreaProvider>
         {showSplash ? (
-            <SplashScreen onFinish={() => setShowSplash(false)} />
+            <SplashScreen
+                onFinish={() => setShowSplash(false)}
+            />
         ) : (
-            <SafeAreaView className="flex-1 bg-white">
-              <View className="flex-1 items-center justify-center">
-                <Text className="text-2xl font-bold text-blue-600">
-                  BantuanKu
-                </Text>
-
-                <Text className="mt-2 text-base text-gray-600">
-                  Welcome to BantuanKu
-                </Text>
-              </View>
-            </SafeAreaView>
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
         )}
       </SafeAreaProvider>
   );
